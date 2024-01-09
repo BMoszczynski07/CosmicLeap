836dd66fbe29438112a73b25ae0a284a9d0f49f0
Bartosz Moszczyński <109615293+BMoszczynski07@users.noreply.github.com>
2024-01-09 16:37:17 +0100

    adding ground and sky

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400)) # width, height
+alien_logo = pygame.image.load("graphics/alien.png")
 pygame.display.set_caption("CosmicLeap")
-# pygame.display.set_icon()
+pygame.display.set_icon(alien_logo)
+clock = pygame.time.Clock()
 
 running = True
+
+sky_surface = pygame.image.load("graphics/Sky.png")
+
+ground_surface = pygame.image.load("graphics/ground.png")
 
 while running:
     for event in pygame.event.get():
@@ -14,6 +20,8 @@ while running:
             pygame.quit()
             exit()
 
-    # draw all our elements
-    # update everything
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+
     pygame.display.update()
+    clock.tick(60) # 60 -> max fps
